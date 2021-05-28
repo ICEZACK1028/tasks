82f53bc254799311b0e3924b4f146169fd37948c
David <david.garcia@lumation.co>
2021-05-28 10:57:08 -0600

    logrado con return string, viewless

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 let texto = 'HolaComoEstamos';
 
-var index= texto.indexOf("o");
+// var array = texto.split();
+// console.log(array);
 
-if(index == -1){
-    console.log('No found');
-}else{
-    console.log(index);
+// var index= array.indexOf("Estamos");
+
+// array.join('');
+
+// console.log(array);
+
+// var index= texto.indexOf("Estamos");
+
+
+function substrings(word1, word2){
+    var words = [word1, word2];
+    var index= texto.indexOf(word1);
+    var index2= texto.indexOf(word2);
+    
+    if(index == -1){
+        console.log('No found');
+    }else{
+        console.log(index, index2);
+    }
 }
+
+substrings("Hola","Como");
+
+// var array = ["test234", "test9495", "test234", "test93992", "test234"];
+// let newArr=[];
+// for (i=0;i<array.length;i++) {
+//   if (array[i].indexOf("test234") >=0 ) {
+//     newArr.push(i);
+//   }
+// }
+// console.log(newArr);
