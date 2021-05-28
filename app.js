ccb61da878188adfd8235c9b1a72e082e8d6d25a
David <david.garcia@lumation.co>
2021-05-28 13:32:58 -0600

    Fprimer función finalizada

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//------------------DESDE CONSOLA------------------
 // let input = 'barfoothefoobarman';
 
 // function substrings([word1, word2]){
@@ -28,45 +29,76 @@
 // substrings(words);
 
 
+//------------------ALERTAS EN NAVEGADOR------------------
+
+// let input = prompt('Enter a word:','');
+
+// function substrings([word1, word2]) {
+
+//     var arrayWords1 = word1 + word2;
+//     var arrayWords2 = word2 + word1;
+
+//     var index = input.indexOf(arrayWords1);
+//     var index2 = input.indexOf(arrayWords2);
+
+//     var arrayIndex = [index, index2];
+
+//     if (index, index2 == -1) {
+//         return alert("No found");
+//     } else if (index < 0) {
+//         arrayIndex[0] = "No found";
+//         return alert(arrayIndex);
+//     } else if (index2 <= -1) {
+//         arrayIndex[1] = "No found"
+//         return alert(arrayIndex);
+//     }
+
+//         alert(arrayIndex);
+// }
+
+// let first = prompt('Send the first word');
+// let second = prompt('Send the second word');
+
+// var words = [first, second];
+
+// substrings(words);
 
 
-let input = prompt('Enter a word:','');
+//------------------CON HTML------------------
 
-function substrings([word1, word2]) {
+function getInput(){
+    var first = document.getElementById("Fword").value; 
+    var second = document.getElementById("Sword").value; 
+
+    var words = [first, second];
+    
+    this.subStrings(words);
+
+}
+
+function subStrings([word1, word2]) {
 
     var arrayWords1 = word1 + word2;
     var arrayWords2 = word2 + word1;
+    
+    var input = document.getElementById('string').value; 
 
     var index = input.indexOf(arrayWords1);
     var index2 = input.indexOf(arrayWords2);
-
+    
     var arrayIndex = [index, index2];
-
-    if (index, index2 == -1) {
-        return alert("No found");
+    
+    if (index == -1 &&  index2 == -1) {
+        return document.getElementById("result").value =  "No found";
     } else if (index < 0) {
         arrayIndex[0] = "No found";
-        return alert(arrayIndex);
+        return document.getElementById("result").value =  arrayIndex;
     } else if (index2 <= -1) {
-        arrayIndex[1] = "No found"
-        return alert(arrayIndex);
+        arrayIndex[1] = "No found";
+        return document.getElementById("result").value =  arrayIndex;
     }
 
-        alert(arrayIndex);
+    return document.getElementById("result").value = arrayIndex;
 }
 
-let first = prompt('Send the first word');
-let second = prompt('Send the second word');
 
-var words = [first, second];
-
-substrings(words);
-
-// var array = ["Hola", "Como", "Hola", "Estamos", "Hola"];
-// let newArr=[];
-// for (i=0;i<array.length;i++) {
-//   if (array[i].indexOf("Hola") >=0 ) {
-//     newArr.push(i);
-//   }
-// }
-// console.log(newArr);
