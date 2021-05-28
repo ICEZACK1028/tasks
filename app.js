5fb9f1ed58bc4152772dbc9225af930d5835a482
David <david.garcia@lumation.co>
2021-05-28 11:59:11 -0600

    AFunción pulida y mejorada

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
-let texto = 'HolaComoEstamos';
+// let input = 'barfoothefoobarman';
 
-// var array = texto.split();
-// console.log(array);
+// function substrings([word1, word2]){
 
-// var index= array.indexOf("Estamos");
+//     var arrayWords1 = word1 + word2;
+//     var arrayWords2 = word2 + word1;
 
-// array.join('');
+//     var index= input.indexOf(arrayWords1);
+//     var index2= input.indexOf(arrayWords2);
 
-// console.log(array);
+//     var arrayIndex = [index, index2];
 
-// var index= texto.indexOf("Estamos");
-
-
-function substrings(word1, word2){
-    var words = [word1, word2];
-    var index= texto.indexOf(word1);
-    var index2= texto.indexOf(word2);
+//     if(index, index2 == -1) {
+//         return console.log("No found");
+//     }else if(index < 0){
+//         arrayIndex[0] = "No found";
+//         return console.log(arrayIndex);
+//     }else if(index2 <= -1){
+//         arrayIndex[1] = "No found"
+//         return console.log(arrayIndex);
+//     }
     
-    if(index == -1){
-        console.log('No found');
-    }else{
-        console.log(index, index2);
+//     console.log(arrayIndex);
+// }
+
+// var words = ["foo", "bar"];
+
+// substrings(words);
+
+
+
+
+let input = prompt('Enter a word:','');
+
+function substrings([word1, word2]) {
+
+    var arrayWords1 = word1 + word2;
+    var arrayWords2 = word2 + word1;
+
+    var index = input.indexOf(arrayWords1);
+    var index2 = input.indexOf(arrayWords2);
+
+    var arrayIndex = [index, index2];
+
+    if (index, index2 == -1) {
+        return alert("No found");
+    } else if (index < 0) {
+        arrayIndex[0] = "No found";
+        return alert(arrayIndex);
+    } else if (index2 <= -1) {
+        arrayIndex[1] = "No found"
+        return alert(arrayIndex);
     }
+
+        alert(arrayIndex);
 }
 
-substrings("Hola","Como");
+let first = prompt('Send the first word');
+let second = prompt('Send the second word');
 
-// var array = ["test234", "test9495", "test234", "test93992", "test234"];
+var words = [first, second];
+
+substrings(words);
+
+// var array = ["Hola", "Como", "Hola", "Estamos", "Hola"];
 // let newArr=[];
 // for (i=0;i<array.length;i++) {
-//   if (array[i].indexOf("test234") >=0 ) {
+//   if (array[i].indexOf("Hola") >=0 ) {
 //     newArr.push(i);
 //   }
 // }
